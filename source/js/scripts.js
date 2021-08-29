1add4e4d2fdb1d44ae7090a19e34ab5c3bea059e
victoria-nsl <vbnaish@yandex.ru>
2021-08-29 14:03:08 +0300

    Добавляет скрипт открытия/закрытия модального окна

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -2,6 +2,10 @@
 const navigationMain = document.querySelector('.main-navigation');
 const navigationToggle = navigationMain.querySelector('.main-navigation__toggle');
 
+const buttonsOrder = document.querySelectorAll('.button--order');
+const overlayPopupSize = document.querySelector('.modal-size__overlay');
+const formPopupSize = document.querySelector('.modal-size__form');
+
 // Кнопка, навигация в планшетной, мобильной версии
 navigationMain.classList.remove('main-navigation--nojs');
 
@@ -14,3 +18,35 @@ navigationToggle.addEventListener('click', () => {
     navigationMain.classList.remove('main-navigation--opened');
   }
 })
+
+/*Открытие окна "Добавить размер" по клику на кнопку "Заказать",
+иконку корзинки на странице Каталога и его закрытие */
+
+if (document.querySelector(".modal-size")) {
+  buttonsOrder.forEach((buttonOrder) => {
+    buttonOrder.addEventListener("click", (evt) => {
+     evt.preventDefault();
+      formPopupSize.classList.add("modal-size__show");
+      overlayPopupSize.classList.add("modal-size__show");
+    });
+  });
+
+  overlayPopupSize.addEventListener("click", (evt) => {
+    if (overlayPopupSize.classList.contains("modal-size__show")) {
+      evt.preventDefault();
+      formPopupSize.classList.remove("modal-size__show");
+      overlayPopupSize.classList.remove("modal-size__show");
+    }
+  });
+
+
+  window.addEventListener("keydown", (evt) => {
+    if (evt.keyCode === 27) {
+      if (overlayPopupSize.classList.contains("modal-size__show")) {
+        evt.preventDefault();
+        formPopupSize.classList.remove("modal-size__show");
+        overlayPopupSize.classList.remove("modal-size__show");
+      }
+    }
+  });
+}
