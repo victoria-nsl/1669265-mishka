b6986eb273cb585c4528358e20ae8912d7d71b59
victoria-nsl <vbnaish@yandex.ru>
2021-09-02 20:24:31 +0300

    Добавляет работу слайдера в разделе Отзывы на главной странице

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -6,6 +6,10 @@ const buttonsOrder = document.querySelectorAll('.button-modal');
 const overlayPopupSize = document.querySelector('.modal-size__overlay');
 const formPopupSize = document.querySelector('.modal-size__form');
 
+const slides = document.querySelectorAll(".feedback__item");
+const buttonBack = document.querySelector('.feedback__toggle--back');
+const buttonForward = document.querySelector('.feedback__toggle--forward');
+
 // Кнопка, навигация в планшетной, мобильной версии
 navigationMain.classList.remove('main-navigation--nojs');
 
@@ -13,10 +17,10 @@ navigationToggle.addEventListener('click', () => {
   if (navigationMain.classList.contains('main-navigation--closed')) {
     navigationMain.classList.remove('main-navigation--closed');
     navigationMain.classList.add('main-navigation--opened');
-  } else {
-    navigationMain.classList.add('main-navigation--closed');
-    navigationMain.classList.remove('main-navigation--opened');
+    return
   }
+  navigationMain.classList.add('main-navigation--closed');
+  navigationMain.classList.remove('main-navigation--opened');
 })
 
 /*Открытие окна "Добавить размер" по клику на кнопку "Заказать",
@@ -24,31 +28,75 @@ navigationToggle.addEventListener('click', () => {
 
 if (document.querySelector(".modal-size")) {
 
-  buttonsOrder.forEach((buttonOrder) => {
-    buttonOrder.addEventListener("click", (evt) => {
-      evt.preventDefault();
-      formPopupSize.classList.add("modal-size__show");
-      overlayPopupSize.classList.add("modal-size-overlay__show");
-    });
-  });
+  const openPopup = () => {
+    formPopupSize.classList.add("modal-size__show");
+    overlayPopupSize.classList.add("modal-size-overlay__show");
+
+    document.addEventListener("keydown", onDocumentEscKeydown);
+    overlayPopupSize.addEventListener("click", onClickOverlayPopupSize);
+  }
 
   const closePopup = () => {
     if (overlayPopupSize.classList.contains("modal-size-overlay__show") && formPopupSize.classList.contains("modal-size__show")) {
       formPopupSize.classList.remove("modal-size__show");
       overlayPopupSize.classList.remove("modal-size-overlay__show");
+
+      document.removeEventListener("keydown", onDocumentEscKeydown);
+      overlayPopupSize.removeEventListener("click", onClickOverlayPopupSize);
     }
   }
 
-  overlayPopupSize.addEventListener("click", (evt) => {
+  const onClickButtonOrder = (evt) => {
     evt.preventDefault();
+    openPopup();
+  };
+
+  const onClickOverlayPopupSize = () => {
     closePopup();
-  });
+  };
 
-
-  window.addEventListener("keydown", (evt) => {
+  const onDocumentEscKeydown = (evt) => {
     if (evt.keyCode === 27) {
       evt.preventDefault();
       closePopup();
     }
+  }
+
+  buttonsOrder.forEach((buttonOrder) => {
+    buttonOrder.addEventListener("click", onClickButtonOrder);
   });
+
+}
+
+//Слайдер Отзывы
+if (document.querySelector(".feedback")) {
+
+  let slideIndex = 1;
+
+  const showSlides = (numberSlide) => {
+    if (numberSlide > slides.length) {
+      slideIndex = 1
+    }
+    if (numberSlide < 1) {
+      slideIndex = slides.length
+    }
+    slides.forEach ((slide) =>  {
+      if (slide.classList.contains("feedback__item--current")) {
+        slide.classList.remove("feedback__item--current");
+      }
+    })
+    slides[slideIndex-1].classList.add("feedback__item--current");
+  }
+
+  const onClickButtonForwarde = () => {
+    showSlides(slideIndex += 1);
+  }
+
+  const onClickButtonBack = () => {
+    showSlides(slideIndex -= 1);
+  }
+
+  buttonBack.addEventListener("click", onClickButtonBack);
+  buttonForward.addEventListener("click", onClickButtonForwarde);
+
 }
